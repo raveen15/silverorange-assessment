3dd203fd2e3afa03acda5b85c7b42284ddd2bb78
raveen15 <raveenth15@gmail.com>
2022-06-30 17:59:49 -0400

    Imported and implemented node-fetch to retrieve JSON data from github URL

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import fetch from 'node-fetch';
 
 export const repos = Router();
 
@@ -15,8 +16,14 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
+  let url = 'https://api.github.com/users/silverorange/repos'
 
-  var filteredData = underscore.where(data, {"fork" : false});
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    res.send(JSON.stringify(out, null, 4));
+  })
 
-  res.send(JSON.stringify(filteredData, null, 4));
+  // var filteredData = underscore.where(data, {"fork" : false});
+  // res.send(JSON.stringify(filteredData, null, 4));
 });
