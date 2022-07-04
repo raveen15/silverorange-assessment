1a115fb315d0abce1e4430cbac6567b11274fb45
raveen15 <raveenth15@gmail.com>
2022-07-04 19:24:09 -0400

    Created an empty table with headings to output repositories from Express API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
 
-export function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component<any, any>{
+  
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <h1>Repositories</h1>
+        <table style={{ width: '100%' }}>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Fork Count</th>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
