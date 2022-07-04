92a7cb670da8e2ca6040e4019322f78737df0a55
raveen15 <raveenth15@gmail.com>
2022-07-04 19:37:01 -0400

    Implemented Express API data to the repository table

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -28,6 +28,20 @@ export default class App extends React.Component<any, any>{
             <th>Language</th>
             <th>Fork Count</th>
           </tr>
+          {this.state.repos.map((repo: {
+            id: number;
+            name: string;
+            description: string;
+            language: string;
+            forks_count: number;
+          }) => 
+            <tr>
+              <td>{repo.name}</td>
+              <td>{repo.description}</td>
+              <td>{repo.language}</td>
+              <td>{repo.forks_count}</td>
+            </tr>
+          )}
         </table>
       </div>
     );
