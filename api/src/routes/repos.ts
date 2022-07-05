2266b9e66d22197f641deb016a2a5c2330a5b2cc
raveen15 <raveenth15@gmail.com>
2022-07-05 18:03:00 -0400

    Fixed duplicate API fetches

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -7,6 +7,17 @@ var localJSONData = require('../../data/repos.json');
 
 const underscore = require("underscore");
 
+let url = 'https://api.github.com/users/silverorange/repos'
+var filteredJSONData = {};
+
+fetch(url)
+.then(res => res.json())
+.then((urlJSONData) => {
+  var concatJSONData = localJSONData.concat(urlJSONData);
+  filteredJSONData = underscore.where(concatJSONData, {"fork" : false});
+})
+.catch(err => {throw err})
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header({
     'Cache-Control' : 'no-store',
@@ -16,14 +27,5 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  let url = 'https://api.github.com/users/silverorange/repos'
-
-  fetch(url)
-  .then(res => res.json())
-  .then((urlJSONData) => {
-    var concatJSONData = localJSONData.concat(urlJSONData);
-    var filteredJSONData = underscore.where(concatJSONData, {"fork" : false});
-    res.send(JSON.stringify(filteredJSONData, null, 4));
-  })
-  .catch(err => {throw err})
+  res.send(JSON.stringify(filteredJSONData, null, 4));
 });
